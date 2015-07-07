5769d5cd0cb656a93e14f892b2b127d68a371cd4
Brennan Moore <brennanmoore@gmail.com>
2015-07-07 10:32:29 -0400

    fix background fade transition

---
+++ assets/application.js
@@ -0,0 +1,2 @@
+
+$(function(){var $background=$('#main-header');var $window=$(window);var updateBackgroundPosition=function(){scrollPosition=$window.scrollTop();console.log(scrollPosition);$background.css({'background-position':"0px -"+scrollPosition+"px"});};$window.scroll(updateBackgroundPosition);});
