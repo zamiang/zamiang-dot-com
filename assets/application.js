dcabddb4caf83b7e3d28a5d97b38c531bb8a1684
Brennan Moore <brennanmoore@gmail.com>
2015-07-07 22:12:32 -0400

    gradient cleanup

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,2 @@
 
-$(function(){var $background=$('#main-header');var $window=$(window);var updateBackgroundPosition=function(){scrollPosition=$window.scrollTop();console.log(scrollPosition);$background.css({'background-position':"0px -"+scrollPosition+"px"});};$window.scroll(updateBackgroundPosition);});
+$(function(){var $background=$('#main-header');var $window=$(window);var updateBackgroundPosition=function(){scrollPosition=$window.scrollTop();$background.css({'background-position':"0px -"+scrollPosition+"px"});};$window.scroll(updateBackgroundPosition);});
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -3,7 +3,6 @@ $(function() {
     var $window = $(window);
     var updateBackgroundPosition = function() {
         scrollPosition = $window.scrollTop();
-        console.log(scrollPosition);
         $background.css({'background-position': "0px -" + scrollPosition + "px"});
     };
     $window.scroll(updateBackgroundPosition);
