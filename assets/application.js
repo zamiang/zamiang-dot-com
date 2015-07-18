82a0ddea797dfde319093a3b787723d80cd5bca7
Brennan Moore <brennanmoore@gmail.com>
2015-07-18 11:36:04 -0400

    remove old styles

--- assets/application.js
+++
@@ -1,2 +0,0 @@
-
-$(function(){var $background=$('#main-header');var $window=$(window);var updateBackgroundPosition=function(){scrollPosition=$window.scrollTop();$background.css({'background-position':"0px -"+scrollPosition+"px"});};$window.scroll(updateBackgroundPosition);});
